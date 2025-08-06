18fe04f0b690aa35e7847f7c0ebc1b6a0fe4ca00
zhpjy <zhpjy@qq.com>
2025-08-06 12:27:02 +0800

    feat:  处理url中的//合并成/的情况

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,18 @@ use axum::{
 use std::env;
 use std::sync::LazyLock;
 use tracing::{info, warn};
+use regex::{self, Regex};
 
 static PASSWORD: LazyLock<String> = LazyLock::new(|| {
     env::var("PASSWORD").unwrap_or_default()
+});
+
+static RE_HTTP: LazyLock<Regex> = LazyLock::new(|| {
+     regex::Regex::new(r"^http:\/").unwrap()
+});
+
+static RE_HTTPS: LazyLock<Regex> = LazyLock::new(|| {
+     regex::Regex::new(r"^https:\/").unwrap()
 });
 
 #[tokio::main]
@@ -56,8 +65,11 @@ async fn handler(Path(path): Path<String>, request: Request) -> impl IntoRespons
     // Remove password from path
     let new_path = &path[PASSWORD.len()..];
     let new_path = (if new_path.is_empty() { "/" } else { new_path }).trim_start_matches('/');
-    let has_protocol = new_path.starts_with("http://") || new_path.starts_with("https://");
-    let new_path_protocol = if has_protocol { new_path } else { &Path(format!("https://{}", new_path))};
+    // Replace leading ^http:\/{1,2} with http:// using regex
+    let new_path = RE_HTTPS.replace(RE_HTTP.replace(new_path, "http://").to_string().as_str(), "https://").to_string();
+    // Some ingress will combine // as /
+    let has_protocol = new_path.starts_with("http:/") || new_path.starts_with("https:/");
+    let new_path_protocol = if has_protocol { new_path } else { format!("https://{}", new_path)};
 
     info!("new path: {}", new_path_protocol);
 
@@ -69,7 +81,7 @@ async fn handler(Path(path): Path<String>, request: Request) -> impl IntoRespons
     let body = request.into_body();
     
     // Call proxy function
-    proxy::proxy_request(method, new_path_protocol, uri.query(), headers, body).await.into_response()
+    proxy::proxy_request(method, new_path_protocol.as_str(), uri.query(), headers, body).await.into_response()
 }
 
 async fn handler_404() -> impl IntoResponse {
